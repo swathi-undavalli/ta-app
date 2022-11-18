ecf12db43bdb7fbcb3dac0b7ce2453d0c84e6f98
Kamesh A <kamesh@diversdashboard.com>
2022-11-19 01:14:28 +0530

    updated minimum SDK number fixed android play store upload issues added bottom sheet for booking get link in booking screen.

---
+++ lib/core/widgets/qr-image.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+
+import 'package:barcode_widget/barcode_widget.dart';
+import 'package:flutter/material.dart';
+
+class QRImage extends StatefulWidget {
+  QRImage({this.height, this.width, this.data, Key key}) : super(key: key);
+
+  double height;
+  double width;
+  String data;
+
+  @override
+  State<QRImage> createState() => _QRImageState();
+}
+
+class _QRImageState extends State<QRImage> {
+  @override
+  Widget build(BuildContext context) {
+    return BarcodeWidget(
+      barcode: Barcode.qrCode(
+        errorCorrectLevel: BarcodeQRCorrectionLevel.high,
+      ),
+      data: widget.data,
+      height: widget.height ?? 260,
+      width: widget.width ?? 260,
+    );
+  }
+}
