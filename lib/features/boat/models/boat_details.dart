7a9e97a93f03b04c1391457233db424401e58e64
Dash & Sim mac mini <kamesh@dashandsim.com>
2024-08-01 12:43:46 +0530

    Released android version 2.0.0+100

--- lib/features/boat/models/boat_details.dart
+++ lib/features/boat/models/boat_details.dart
@@ -44,49 +44,48 @@ class BoatDetails {
   factory BoatDetails.fromMap(Map<String, dynamic>? json, List<String?>? bookingDate) {
     if (json == null) return BoatDetails();
 
-    // List<Instructor>? instructors;
+    List<Instructor>? instructors;
 
-    // if (((json['instructors'] as List?) ?? []).length == 1) {
-    //   Instructor ins = Instructor.fromMap(json['instructors'][0]);
-    //
-    //   if (((json['instructorTanks'] ?? {}) as Map).isNotEmpty) {
-    //     (json['instructorTanks'] as Map<String, dynamic>).forEach((date, tankInfo) {
-    //       instructors ??= [];
-    //       var i = ins.copyWith(
-    //         date: date,
-    //         air: tankInfo['air'] ?? 0,
-    //         nitrox: tankInfo['nitrox'] ?? 0,
-    //       );
-    //
-    //       instructors?.add(i);
-    //     });
-    //   }
-    //
-    //   // instructorTanks and date in the instructor model is null
-    //   else if (((json['instructorTanks'] ?? {}) as Map).isEmpty && ins.date == null) {
-    //     for (var date in (bookingDate ?? [])) {
-    //       instructors ??= [];
-    //       var i = ins.copyWith(
-    //         date: date,
-    //         air: ins.air ?? 0,
-    //         nitrox: ins.nitrox ?? 0,
-    //       );
-    //
-    //       instructors.add(i);
-    //     }
-    //   }
-    // }
+    if (((json['instructors'] as List?) ?? []).length == 1) {
+      Instructor ins = Instructor.fromMap(json['instructors'][0]);
+
+      if (((json['instructorTanks'] ?? {}) as Map).isNotEmpty) {
+        (json['instructorTanks'] as Map<String, dynamic>).forEach((date, tankInfo) {
+          instructors ??= [];
+          var i = ins.copyWith(
+            date: date,
+            air: tankInfo['air'] ?? 0,
+            nitrox: tankInfo['nitrox'] ?? 0,
+          );
+
+          instructors?.add(i);
+        });
+      }
+
+      // instructorTanks and date in the instructor model is null
+      else if (((json['instructorTanks'] ?? {}) as Map).isEmpty && ins.date == null) {
+        for (var date in (bookingDate ?? [])) {
+          instructors ??= [];
+          var i = ins.copyWith(
+            date: date,
+            air: ins.air ?? 0,
+            nitrox: ins.nitrox ?? 0,
+          );
+
+          instructors.add(i);
+        }
+      }
+    }
 
     return BoatDetails(
       boat: json['boat'] ?? {},
       status: json['status'] ?? {},
       employeeNotes: json['employeeNotes'],
       bookingStatus: json['bookingStatus'],
-      instructors:
-          // instructors ??
+      instructors: instructors ??
           List<Instructor>.from(
-        (json['instructors'] ?? []).map((x) => Instructor.fromMap(x)),
-      ),
+            (json['instructors'] ?? []).map((x) => Instructor.fromMap(x)),
+          ),
       diveBuddies: List<Instructor>.from(
         (json['diveBuddies'] ?? []).map((x) => Instructor.fromMap(x)),
       ),
