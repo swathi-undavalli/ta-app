b1097b5a68799370afd6ce3e680c9f122f2ec1dc
Kamesh Adadadi <wb.kamesh@gmail.com>
2025-08-31 12:10:09 +0530

    changed version in main to publish it in app store

--- lib/features/dive_checklist/presentation/views/dive_checklist_view.dart
+++ lib/features/dive_checklist/presentation/views/dive_checklist_view.dart
@@ -1,7 +1,6 @@
 import 'dart:developer';
 import 'dart:io';
 import 'dart:ui' as ui;
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
