e7fa38fcbcea94915f8bd83c5b02800b5be32545
Kamesh Adadadi <wb.kamesh@gmail.com>
2025-09-05 22:52:25 +0530

    Published android version 2.0.0+126 and ios version 1.1.3+42

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ import 'core/app/app.dart';
 import 'core/services/notification_service.dart';
 import 'features/messaging/firebase_messaging_controller.dart';
 
-String currentIosVersion = '1.1.3+41';
+String currentIosVersion = '1.1.3+42';
 
 bool get isIos => Platform.isIOS;
 
